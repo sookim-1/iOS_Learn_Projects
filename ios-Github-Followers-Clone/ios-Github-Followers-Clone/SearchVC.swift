355ecc4862aaa40640f6246217c2f05c0e7e47a9
Sookim18 <scstnghks@naver.com>
2021-10-22 17:40:40 +0900

    💄 [UI] : SearchVC(검색화면) - UI요소 추가

--- ios-Github-Followers-Clone/ios-Github-Followers-Clone/SearchVC.swift
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/SearchVC.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+    }
+    
 }
