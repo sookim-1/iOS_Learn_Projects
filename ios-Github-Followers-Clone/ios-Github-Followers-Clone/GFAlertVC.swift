817264d418a5a9aedd2267fab34258b83fccd99b
Sookim18 <scstnghks@naver.com>
2021-10-22 21:39:17 +0900

    💄 [UI] : CustomeAlert뷰컨트롤러 추가

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/GFAlertVC.swift
@@ -0,0 +1,67 @@
+//
+//  GFAlertVC.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2021/10/22.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    }
+
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func configureTitleLabel() {
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
+    }
+    
+    func configureActionButton() {
+        containerView.addSubview(actionButton)
+        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+    }
+    
+    func configureBodyLabel() {
+        containerView.addSubview(messageLabel)
+        messageLabel.text = message ?? "Unable to complete request"
+        messageLabel.numberOfLines = 4
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+    
+}
