b6ea6ec1eea5bdbb476581d2c3fb86e1ddfff78d
Sookim18 <scstnghks@naver.com>
2021-10-22 20:41:13 +0900

    ✨ [Feat] : SearchVC - 텍스트필드 유효성검사 및 String 정규표현식 추가

--- ios-Github-Followers-Clone/ios-Github-Followers-Clone/SearchVC.swift
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/SearchVC.swift
@@ -13,6 +13,10 @@ class SearchVC: UIViewController {
     let usernameTextField = GFTextField()
     let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
     
+    var isUsernameEntered: Bool {
+        return !usernameTextField.text!.isEmpty
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -34,6 +38,9 @@ class SearchVC: UIViewController {
     }
     
     @objc func pushFollowerListVC() {
+        
+        guard isUsernameEntered else { return }
+        
         let followerListVC = FollowerListVC()
         followerListVC.username = usernameTextField.text
         followerListVC.title = usernameTextField.text
