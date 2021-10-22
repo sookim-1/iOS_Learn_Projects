db8844ea7577ea728f4ba5c6f05f8ef9d764f22a
Sookim18 <scstnghks@naver.com>
2021-10-22 22:20:35 +0900

    ✨ [Feat] : UIViewController+Ext 추가 - Alert창 띄우는 메서드정의

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/UIViewController+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Ext.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2021/10/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
