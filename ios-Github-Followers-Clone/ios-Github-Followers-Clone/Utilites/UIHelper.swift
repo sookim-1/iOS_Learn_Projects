f35c446d222a06540298439f64240ba56f7ebe68
Sookim18 <scstnghks@naver.com>
2021-10-24 06:27:36 +0900

    ✨ [Feat] : FollowerListVC -> createThreeColumnFlowLayout메서드 UIHelper구조체로 분리

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Utilites/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2021/10/24.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+}
