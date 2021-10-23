ac3a4770dea7e58a5180375cc9f8deacf4f05698
Sookim18 <scstnghks@naver.com>
2021-10-24 03:55:13 +0900

    💄 [UI] : FollowerCell - 커스텀셀 오토레이아웃 지정

--- ios-Github-Followers-Clone/ios-Github-Followers-Clone/Custom Views/Cells/FollowerCell.swift
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Custom Views/Cells/FollowerCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
     
-    let avatarImageView = UIImageView()
+    let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
     
     override init(frame: CGRect) {
@@ -29,5 +29,19 @@ class FollowerCell: UICollectionViewCell {
     private func configure() {
         addSubview(avatarImageView)
         addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
 }
