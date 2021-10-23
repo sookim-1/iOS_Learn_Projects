ec3fd09b7048d9e684b494b877e79e902301b29c
Sookim18 <scstnghks@naver.com>
2021-10-24 01:26:33 +0900

    🐛 [Fix] : FollowerListVC -> SearchVC로 스와이프 할 때 네비게이션바 사라지는 문제, 키보드입력 대기 문제 해결

--- ios-Github-Followers-Clone/ios-Github-Followers-Clone/Screens/SearchVC.swift
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Screens/SearchVC.swift
@@ -29,7 +29,7 @@ class SearchVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     func createDismissKeyboardTapGesture() {
@@ -48,6 +48,8 @@ class SearchVC: UIViewController {
         followerListVC.username = usernameTextField.text
         followerListVC.title = usernameTextField.text
         navigationController?.pushViewController(followerListVC, animated: true)
+        
+        self.view.endEditing(true)
     }
     
     func configureLogoImageView() {
