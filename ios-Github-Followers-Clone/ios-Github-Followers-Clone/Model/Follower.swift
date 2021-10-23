5b20db8fcd82ec0b1a8664fd5aa6e40dbc1c6e0b
Sookim18 <scstnghks@naver.com>
2021-10-24 00:59:50 +0900

    ✨ [Feat] : JSON 파싱모델 추가 - User,Follower

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2021/10/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
