7af4338d185b0b61dd1bb6669abdacc03c6bcebe
Sookim18 <scstnghks@naver.com>
2022-01-24 16:41:47 +0900

    ✨ [Feat] : Firebase 관련 기본 파일추가

---
+++ ios-chatting-app/ios-chatting-app/CollectionReference.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionReference.swift
+//  ios-chatting-app
+//
+//  Created by sookim on 2022/01/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+
+enum FCollectionReference: String {
+    case User
+    case Typing
+    case Recent
+    case Message
+    case Group
+    case Call
+}
+
+
+func reference(_ collectionReference: FCollectionReference) -> CollectionReference{
+    return Firestore.firestore().collection(collectionReference.rawValue)
+}
---
+++ ios-chatting-app/ios-chatting-app/HelperFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  HelperFunctions.swift
+//  ios-chatting-app
+//
+//  Created by sookim on 2022/01/24.
+//
+
+import Foundation
+import UIKit
+import FirebaseFirestore
+
+//MARK: GLOBAL FUNCTIONS
+private let dateFormat = "yyyyMMddHHmmss"
+
+func dateFormatter() -> DateFormatter {
+    
+    let dateFormatter = DateFormatter()
+    
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: TimeZone.current.secondsFromGMT())
+    
+    dateFormatter.dateFormat = dateFormat
+    
+    return dateFormatter
+}
