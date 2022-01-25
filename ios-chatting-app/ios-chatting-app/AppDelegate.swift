4833bc020b23d1d0d297aa4591369bb1baeada64
Sookim18 <scstnghks@naver.com>
2022-01-26 03:00:58 +0900

    ✨ [Feat] : 위치보내기 - CLLocationManager 초기설정

--- ios-chatting-app/ios-chatting-app/AppDelegate.swift
+++ ios-chatting-app/ios-chatting-app/AppDelegate.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import Firebase
+import CoreLocation
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var authListener: AuthStateDidChangeListenerHandle?
+    var locationManager: CLLocationManager?
+    var coordinates: CLLocationCoordinate2D?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -58,6 +61,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // MARK: - Location manger
+    
+    func locationManagerStart() {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBest // 원하는 정확도 상수값
+            locationManager!.requestWhenInUseAuthorization() // 사용자에게 권한 요청
+        }
+        
+        locationManager!.startUpdatingLocation()
+    }
+
+    func locationMangerStop() {
+        
+        if locationManager != nil {
+            locationManager!.stopUpdatingLocation()
+        }
+    }
+    
+    // MARK: - Location Manager delegate
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print("faild to get location")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        // 사용자 권한 상태에 따라 분기처리
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            manager.startUpdatingLocation()
+        case .authorizedAlways:
+            manager.startUpdatingLocation()
+        case .restricted:
+            print("restricted")
+        case .denied:
+            locationManager = nil
+            print("denied location access")
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        coordinates = locations.last!.coordinate
+    }
 
 }
 
