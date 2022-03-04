4c9ca85416f7a58930b2829c3452fc419c79946c
Sookim18 <scstnghks@naver.com>
2022-03-04 23:19:19 +0900

    💄 [UI] : 개인유저화면 추가

--- ios-Github-Followers-Clone/ios-Github-Followers-Clone/Screens/FollowerListVC.swift
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Screens/FollowerListVC.swift
@@ -18,6 +18,7 @@ class FollowerListVC: UIViewController {
     var filterdFollowers: [Follower] = []
     var page: Int = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -114,17 +115,31 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filterdFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destVC = UserInfoVC()
+        destVC.username = follower.login
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
 }
 
 extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
-        
+        isSearching = true
         filterdFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filterdFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        //isSearching = false
+        isSearching.toggle()
         updateData(on: followers)
     }
+
 }
+
