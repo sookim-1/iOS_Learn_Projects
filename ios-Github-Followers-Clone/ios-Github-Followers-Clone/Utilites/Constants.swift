a8c7bfad0437fa5e925f37b5b8e5167ff0334dd4
Sookim18 <scstnghks@naver.com>
2022-03-07 07:20:36 +0900

    💄 [UI] : UserInfoHeaderVC 추가

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Utilites/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2022/03/07.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
