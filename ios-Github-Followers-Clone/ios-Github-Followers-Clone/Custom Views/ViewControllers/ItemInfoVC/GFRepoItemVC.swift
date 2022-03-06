5e68aaf6ac3aa412f840eb1aabc4d252917b5843
Sookim18 <scstnghks@naver.com>
2022-03-07 08:38:32 +0900

    💄 [UI] : UserInfoVC 아이템화면 구현

---
+++ ios-Github-Followers-Clone/ios-Github-Followers-Clone/Custom Views/ViewControllers/ItemInfoVC/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  ios-Github-Followers-Clone
+//
+//  Created by sookim on 2022/03/07.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
