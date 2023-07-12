76abecdafe9959764034d4630a9b8bf76649607a
sookim-1 <scstnghks@naver.com>
2023-07-12 22:30:37 +0900

    [UI] : 로그인 화면 UI 구현

---
+++ UberCloneSwiftUI/UberCloneSwiftUI/UberCloneSwiftUI/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,173 @@
+//
+//  LoginView.swift
+//  UberCloneSwiftUI
+//
+//  Created by 수킴 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                VStack(spacing: -16) {
+                
+                // image
+                Image(systemName: "car")
+                    .resizable()
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 200)
+                    .scaledToFill()
+                
+                // title
+                Text("UBER")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+                // input fields
+                
+                VStack(spacing: 32) {
+                    // email
+                    VStack(alignment: .leading, spacing: 12) {
+                        // title
+                        Text("이메일")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.footnote)
+                        
+                        // text fields
+                        TextField("이메일을 입력해주세요", text: $email)
+                            .foregroundColor(.white)
+                    
+                        // divider
+                        Rectangle()
+                            .foregroundColor(Color(.init(white: 1, alpha: 0.3)))
+                            .frame(width: UIScreen.main.bounds.width - 32, height: 0.7)
+                    }
+                    
+                    
+                    // password
+                    VStack(alignment: .leading, spacing: 12) {
+                        // title
+                        Text("비밀번호")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.footnote)
+                        
+                        // text fields
+                        TextField("비밀번호를 입력해주세요", text: $password)
+                            .foregroundColor(.white)
+                    
+                        // divider
+                        Rectangle()
+                            .foregroundColor(Color(.init(white: 1, alpha: 0.3)))
+                            .frame(width: UIScreen.main.bounds.width - 32, height: 0.7)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("비밀번호를 잊어버리셨나요?")
+                            .font(.system(size: 13, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding(.top)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+
+                // social sign in view
+                VStack {
+                    // diveders + text
+                    HStack(spacing: 24) {
+                        Rectangle()
+                            .frame(width: 76, height: 1)
+                            .foregroundColor(.white)
+                            .opacity(0.5)
+                        
+                        Text("SNS 로그인")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        
+                        Rectangle()
+                            .frame(width: 76, height: 1)
+                            .foregroundColor(.white)
+                            .opacity(0.5)
+                    }
+                    
+                    // sign up button
+                    HStack(spacing: 24) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "f.circle")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 44, height: 44)
+                        }
+
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "g.circle")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 44, height: 44)
+                        }
+                    }
+                }
+                .padding(.vertical)
+                
+                Spacer()
+                
+                // sign in button
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("로그인")
+                            .foregroundColor(.black)
+                        
+                        Image(systemName: "arrow.right")
+                            .foregroundColor(.black)
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                .background(Color(.white))
+                .cornerRadius(10)
+
+                Spacer()
+                
+                // sign up button
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("계정이 생각나지 않아요!")
+                            .font(.system(size: 14))
+                        
+                        Text("회원가입")
+                            .font(.system(size: 14, weight: .semibold))
+                    }
+                    .foregroundColor(.white)
+                }
+
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ UberCloneSwiftUI/UberCloneSwiftUI/UberCloneSwiftUI/Core/Authentication/Views/RegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationView.swift
+//  UberCloneSwiftUI
+//
+//  Created by 수킴 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
