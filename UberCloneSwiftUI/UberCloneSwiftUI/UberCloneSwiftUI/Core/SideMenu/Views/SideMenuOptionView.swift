a40663447b11249a714bc2efaa23780653d1765a
sookim-1 <scstnghks@naver.com>
2023-07-13 22:40:18 +0900

    [UI] : 사이드메뉴 옵션 리스트 화면 구현

---
+++ UberCloneSwiftUI/UberCloneSwiftUI/UberCloneSwiftUI/Core/SideMenu/Views/SideMenuOptionView.swift
@@ -0,0 +1,32 @@
+//
+//  SideMenuOptionView.swift
+//  UberCloneSwiftUI
+//
+//  Created by 수킴 on 2023/07/13.
+//
+
+import SwiftUI
+
+struct SideMenuOptionView: View {
+    
+    let viewModel: SideMenuOptionViewModel
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: viewModel.imageName)
+                .font(.title2)
+                .imageScale(.medium)
+            
+            Text(viewModel.title)
+                .font(.system(size: 16, weight: .semibold))
+            
+            Spacer()
+        }
+    }
+}
+
+struct SideMenuOptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuOptionView(viewModel: .trips)
+    }
+}
