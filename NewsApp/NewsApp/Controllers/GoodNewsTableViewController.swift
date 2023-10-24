0eee63a3100ae45648e88d3cff14a273b6235bbd
sookim <scstnghks@naver.com>
2023-10-24 22:16:55 +0900

    [Rx] : NewsApp - 변환연산자를 통해 API 응답 구현

---
+++ NewsApp/NewsApp/Controllers/GoodNewsTableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  GoodNewsTableViewController.swift
+//  NewsApp
+//
+//  Created by sookim on 10/24/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class GoodNewsTableViewController: UITableViewController {
+
+    private let disposeBag = DisposeBag()
+    private var articles = [Article]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Good News"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.tableView.register(GoodNewsTableViewCell.self, forCellReuseIdentifier: GoodNewsTableViewCell.reuseID)
+        self.refactorPopulateNews()
+    }
+    
+    private func populateNews() {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=0cf790498275413a9247f8b94b3843fd")!
+        
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                
+                return URLSession.shared.rx.data(request: request)
+            }
+            .map { data -> [Article]? in
+                return try? JSONDecoder().decode(ArticlesList.self, from: data).articles
+            }
+            .subscribe(onNext: { [weak self] articles in
+                guard let self
+                else { return }
+                
+                if let articles {
+                    self.articles = articles
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    // URLRequest+Extension 활용
+    private func refactorPopulateNews() {
+        URLRequest.load(resource: ArticlesList.all)
+            .subscribe(onNext: { [weak self] result in
+                guard let self
+                else { return }
+                
+                if let result {
+                    self.articles = result.articles
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GoodNewsTableViewCell.reuseID, for: indexPath) as! GoodNewsTableViewCell
+        
+        let item = articles[indexPath.row]
+        cell.configureUI(article: item)
+
+        return cell
+    }
+
+}
---
+++ NewsApp/NewsApp/README.md
@@ -0,0 +1,8 @@
+#  뉴스 API 정보
+
+- URL 주소 : https://newsapi.org/
+    - 앱에서 참고한 URL : https://newsapi.org/docs/endpoints/top-headlines
+
+
+## 유용한 링크
+- 색상 RGB 값 편리하게 구하는 사이트 : https://flatuicolors.com/
