8c9c051d45e34a197029dead1ef81aa89f5e4f21
sookim <scstnghks@naver.com>
2023-10-24 22:51:44 +0900

    [Rx] : 에러 처리

---
+++ GoodWeather/GoodWeather/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,51 @@
+//
+//  URLRequest+Extensions.swift
+//  GoodWeather
+//
+//  Created by sookim on 10/24/23.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.just(resource.url)
+            .flatMap { url -> Observable<(response: HTTPURLResponse, data: Data)> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.response(request: request)
+            }.map { response, data -> T in
+                
+                // ~= : statusCode가 200~300 범위 안에 있는지 확인하는 연산자
+                if 200..<300 ~= response.statusCode {
+                    return try JSONDecoder().decode(T.self, from: data)
+                } else {
+                    throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+                }
+                
+        }.asObservable()
+        
+    }
+    
+    /*
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+        }.asObservable()
+        
+    } */
+    
+}
